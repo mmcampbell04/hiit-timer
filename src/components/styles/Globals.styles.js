8684b7824280147685fad0816ba9669ec51cd38c
Meghan McCall-Campbell <meghan.mccall.campbell@gmail.com>
2022-04-28 17:51:08 +0100

    added global styles

---
+++ src/components/styles/Globals.styles.js
@@ -0,0 +1,86 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+
+  h1, h2, h3, p {
+    margin: 0;
+    padding: 0;
+  }
+  
+  body {
+    width: 100%;
+    font-family: 'Akshar', sans-serif;
+    font-weight: 500;
+    font-size: 100%;
+    text-rendering: optimizeSpeed;
+    line-height: 1.5;
+    background: #000000;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to left, #434343, #000000);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to left, #434343, #000000); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+   
+    
+  }
+
+  /* Remove styles from a elements */
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
+ul, li {
+  list-style: none;
+}
+
+/* Make images easier to work with */
+
+img,
+picture {
+  max-width: 100%;
+  display: block;
+}
+
+
+/* Inherit fonts for inputs and buttons */
+input,
+button,
+textarea,
+select {
+  font: inherit;
+}
+
+/* Remove default button & fieldset styles */
+button,
+fieldset {
+  border: none;
+  background: none;
+}
+
+/* Remove all animations, transitions and smooth scroll for people that prefer not to see them */
+@media (prefers-reduced-motion: reduce) {
+  html:focus-within {
+    scroll-behavior: auto;
+  }
+
+  *,
+  *::before,
+  *::after {
+    animation-duration: 0.01ms !important;
+    animation-iteration-count: 1 !important;
+    transition-duration: 0.01ms !important;
+    scroll-behavior: auto !important;
+  }
+}
+
+
+
+`;
+
+export default GlobalStyle;
